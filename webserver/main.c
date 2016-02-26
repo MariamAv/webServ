01ecd54cb1251ca231d4139d4f9c2d97393fe6ed
Mariam AVANESSOVA <mariam.avanessova@etudiant.univ-lille1.fr>
2016-02-26 10:26:12 +0100

    m

--- webserver/main.c
+++ webserver/main.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include "socket.h"
 
-int main (int argc, char **argv)
+int main ()
 {
   
 	/* Arnold Robbins in the LJ of February ’95 , describing RCS */
-	if (argc > 1 && strcmp(argv[1], "-advice") == 0) {
+	/*if (argc > 1 && strcmp(argv[1], "-advice") == 0) {
 		printf("Don’t Panic !\n");
 		return 42;
 	}
 		printf("Need an advice ?\n");
-		return 0;
+		return 0;*/
 
-	create_server(8080);
-	create_client();
-	
+
+
+int socket_server;
+int create_server();
+int create_client() {
+
+	int socket_client;
+	socket_client = accept(socket_server, NULL, NULL);
+	if (socket_client == -1) {
+		perror("accept");
+		return -1;
+	}
+	printf("BONJOUR BOB\n");
+	return 0;
+}
+
+	return 0;
 }
