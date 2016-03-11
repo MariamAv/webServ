6884613d517f7640fd6e05c10249d06453cf5748
GoldenDracoa <cailliez.alexandre@hotmail.fr>
2016-03-11 11:37:17 +0100

    Fonction signal

--- webserver/main.c
+++ webserver/main.c
@@ -18,24 +18,23 @@ int main ()
 		printf("Need an advice ?\n");
 		return 0;*/
 
-	if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
-	{
-		perror(" signal ");
-	}
 
+	initialiser_signaux();
 	int socket_server;
 	if ((socket_server = create_server(8080)) == -1) {
 		perror("socket error");
 		return -1;
 	}
 
-	int socket_client;
-	if ((socket_client = accept(socket_server, NULL, NULL)) == -1) {
+	int socket_client = accept(socket_server, NULL, NULL);
+
+	if (socket_client == -1) {
 		perror("Connection refused");
 		return -1;
 	} 
 
 	puts("Client connected\n");
+
 
 	const char* message = "\n __      __      ___.     _________                   \n/  \\    /  \\ ____\\_ |__  /   _____/ ______________  __\n\\   \\/\\/   // __ \\| __ \\ \\_____  \\_/ __ \\_  __ \\  \\/ /\n \\        /\\  ___/| \\_\\ \\/        \\  ___/|  | \\/\\   / \n  \\__/\\  /  \\___  >___  /_______  /\\___  >__|    \\_/  \n       \\/       \\/    \\/        \\/     \\/             \n                                            ________________________\n  _______________________-------------------                        `\\\n /:--__                                                              |\n||< > |                                   ___________________________/\n| \\__/_________________-------------------                         |\n|                                                                  |\n |                             { WELCOME }                         |\n |                                                                  |\n |                        Bonjour cher Client,                      |\n  |        Vous êtes désormais connecté à ce magnifique serveur !    |\n  |      Grâce à ce serveur vous pouvez envoyer des messages         |\n  |        à vous même et faire pleins d'autres trucs,               |\n  |      à vous de le découvrir ! ;)                                  |\n   |                                                                  |\n   |                               /)/)                               |\n   |                              (^.^)                              |\n  |                               (n_n)o         ____________________|_\n  |  ___________________-------------------------                      `\\\n  |/`--_                                                                 |\n  ||[ ]||                                            ___________________/\n   \\===/___________________--------------------------\n\n\n"; 
 
@@ -59,4 +58,11 @@ int main ()
 	puts("Connection Closed\n");
 
 	return 0;
+}
+
+void initialiser_signaux(void) { 	
+	printf("signaux init\n");
+	if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
+		perror(" signal ");
+	}		
 }
