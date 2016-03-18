f9a92881d545a1050b39e25e146a7b9f88153259
Mariam AVANESSOVA <mariam.avanessova@etudiant.univ-lille1.fr>
2016-03-18 10:37:25 +0100

    sigpipe not ignored (?)

--- webserver/main.c
+++ webserver/main.c
@@ -18,8 +18,8 @@ int main ()
 		printf("Need an advice ?\n");
 		return 0;*/
 
-
 	initialiser_signaux();
+
 	int socket_server;
 	if ((socket_server = create_server(8080)) == -1) {
 		perror("socket error");
@@ -27,14 +27,12 @@ int main ()
 	}
 
 	int socket_client = accept(socket_server, NULL, NULL);
-
 	if (socket_client == -1) {
-		perror("Connection refused");
+		perror("Connection Refused");
 		return -1;
 	} 
 
-	puts("Client connected\n");
-
+	puts("Client Connected\n");
 
 	const char* message = "\n __      __      ___.     _________                   \n/  \\    /  \\ ____\\_ |__  /   _____/ ______________  __\n\\   \\/\\/   // __ \\| __ \\ \\_____  \\_/ __ \\_  __ \\  \\/ /\n \\        /\\  ___/| \\_\\ \\/        \\  ___/|  | \\/\\   / \n  \\__/\\  /  \\___  >___  /_______  /\\___  >__|    \\_/  \n       \\/       \\/    \\/        \\/     \\/             \n                                            ________________________\n  _______________________-------------------                        `\\\n /:--__                                                              |\n||< > |                                   ___________________________/\n| \\__/_________________-------------------                         |\n|                                                                  |\n |                             { WELCOME }                         |\n |                                                                  |\n |                        Bonjour cher Client,                      |\n  |        Vous êtes désormais connecté à ce magnifique serveur !    |\n  |      Grâce à ce serveur vous pouvez envoyer des messages         |\n  |        à vous même et faire pleins d'autres trucs,               |\n  |      à vous de le découvrir ! ;)                                  |\n   |                                                                  |\n   |                               /)/)                               |\n   |                              (^.^)                              |\n  |                               (n_n)o         ____________________|_\n  |  ___________________-------------------------                      `\\\n  |/`--_                                                                 |\n  ||[ ]||                                            ___________________/\n   \\===/___________________--------------------------\n\n\n"; 
 
@@ -45,6 +43,7 @@ int main ()
 
 	char msg_ent[100];
 	int lecture;
+	initialiser_signaux() ;
 	while((lecture = recv(socket_client, msg_ent, sizeof(msg_ent), 0))>0){
 		if (write(socket_client, msg_ent, strlen(msg_ent)) == -1) {
 			perror("write2 error");
@@ -61,7 +60,7 @@ int main ()
 }
 
 void initialiser_signaux(void) { 	
-	printf("signaux init\n");
+	puts("Signal Initialized\n");
 	if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
 		perror(" signal ");
 	}		
